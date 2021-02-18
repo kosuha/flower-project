12a6e12e47c52f34c4df12bd8f51fae2a43b804d
Seonho Kim <okeydokekim@gmail.com>
2021-02-18 10:50:15 +0900

    commit

--- scenegraph.js
+++ scenegraph.js
@@ -9,7 +9,7 @@ const near = 0.1; // 카메라 앞에 렌더링되는 공간 범위 (near에서 
 const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); // 카메라 생성
 
-camera.position.set(0, 50, 0); // 카메라 포지션 조정
+camera.position.set(0, 20, 0); // 카메라 포지션 조정
 camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
 
@@ -19,17 +19,56 @@ setLight();
 // 회전값을 업데이트할 객체들
 const objects = [];
 
+// 태양계 지역공간 생성
+const solarSystem = new THREE.Object3D();
+scene.add(solarSystem);
+objects.push(solarSystem);
+
+// 지구 궤도 공간 생성
+const earthOrbit = new THREE.Object3D();
+earthOrbit.position.x = 10;
+solarSystem.add(earthOrbit);
+objects.push(earthOrbit);
+
+// 달 궤도 공간 생성
+const moonOrbit = new THREE.Object3D();
+moonOrbit.position.x = 2;
+earthOrbit.add(moonOrbit);
+
 // 하나의 geometry로 모든 태양, 지구, 달을 생성
 const radius = 1;
 const widthSegments = 6;
 const heightSegments = 6;
 const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
 
+// 태양 추가
 const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
 const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
 sunMesh.scale.set(5, 5, 5);  // 태양의 크기를 키움
-scene.add(sunMesh);
+solarSystem.add(sunMesh);
 objects.push(sunMesh);
+
+// 지구 추가
+const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x2233FF, emissive: 0x112244 });
+const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+earthMesh.position.x = 10;
+solarSystem.add(earthMesh);
+objects.push(earthMesh);
+
+// 달 추가
+const moonMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, emissive: 0x222222 });
+const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+moonMesh.scale.set(.2, .2, .2);
+moonOrbit.add(moonMesh);
+objects.push(moonMesh);
+
+// AxesHelper 클래스(지역 공간의 xyz축을 표시)를 각 요소에 지정
+objects.forEach((node) => {
+    const axes = new THREE.AxesHelper();
+    axes.material.depthTest = false;
+    axes.renderOrder = 1;
+    node.add(axes);
+});
 
 renderer.render(scene, camera); // 렌더링
 
